71d29170cefa2e02f418be0748ce6dd02bc04dbc
alexSam <alexsamalot19@gmail.com>
2020-01-07 17:07:10 -0500

    build fix

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static(path.join("client", "build")));
 }
 // Add routes, both API and view
 app.use(routes);
