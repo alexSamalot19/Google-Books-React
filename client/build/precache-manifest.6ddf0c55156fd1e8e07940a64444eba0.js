eb91343a437cfa10cc9803fb8ae471f9661e4b2c
alexSam <alexsamalot19@gmail.com>
2020-01-07 16:40:04 -0500

    build fix

---
+++ client/build/precache-manifest.6ddf0c55156fd1e8e07940a64444eba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3978d8fe7f87720376b6",
+    "url": "/static/js/main.78877e70.chunk.js"
+  },
+  {
+    "revision": "0cbd7f428dfba41ceb79",
+    "url": "/static/js/2.573d3b89.chunk.js"
+  },
+  {
+    "revision": "3978d8fe7f87720376b6",
+    "url": "/static/css/main.8c0eecd6.chunk.css"
+  },
+  {
+    "revision": "4675bd8ac4ce706be9350b0d8ae45bad",
+    "url": "/index.html"
+  }
+];
