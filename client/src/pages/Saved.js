a1fc9e6cce73e1b64d4d1e17ff4f4e9cb68f0d9b
alexSam <alexsamalot19@gmail.com>
2020-01-07 15:28:10 -0500

    minimum viable product

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -79,12 +79,50 @@ class Saved extends Component {
               <List>
                 {this.state.books.map(book => (
                   <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
+                    {/*  <Link to={"/books/" + book._id}>
                       <strong>
                         {book.title} by {book.author}
                       </strong>
                     </Link>
-                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                    <DeleteBtn onClick={() => this.deleteBook(book._id)} /> */}
+                    <Row
+                      className="SearchResult row"
+                      id={book.title + "Card"}
+                      key={book._id}
+                    >
+                      {/* col-3 show image of the book */}
+                      <Col size="2" className="bookImage">
+                        <img src={book.image} alt={book.title} />
+                      </Col>
+                      <Col size="1" className="emptyCol" />
+                      {/* col-9 show information of the book */}
+                      <Col size="9" className="bookInfo">
+                        <Row>
+                          <h3 className="bookTitle">{book.title}</h3>
+                        </Row>
+                        <Row>
+                          <h4 className="bookAuthor">{book.authors}</h4>
+                        </Row>
+                        <Row>
+                          <p className="bookDescription">{book.description}</p>
+                        </Row>
+                      </Col>
+                    </Row>
+                    <br></br>
+                    <Row className="buttonDiv ">
+                      <button
+                        className="saveBook btn btn-primary"
+                        id={book.id}
+                        onClick={() => this.deleteBook(book._id)}
+                      >
+                        Delete Book
+                      </button>
+                      <a href={book.link} target="_blank">
+                        <button className="viewBook btn btn-success">
+                          View Book
+                        </button>
+                      </a>
+                    </Row>
                   </ListItem>
                 ))}
               </List>
